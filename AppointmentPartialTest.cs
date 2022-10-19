b3f219420024ee9243af4d9d45ae6233649a7e29
KrakenZim <104575615+KrakenZim@users.noreply.github.com>
2022-10-19 13:46:26 -0400

    Still doesn't work properly

--- AppointmentPartialTest.cs
+++ AppointmentPartialTest.cs
@@ -8,15 +8,37 @@ namespace VetClinic
 {
     public partial class Appointment
     {
+        private object selectedItem1;
+        private object selectedItem2;
+        private object selectedItem3;
+        private string text;
+        private object text1;
+
         public Appointment(int vet_id, int owner_id, int pet_id, DateTime when, string note)
         {
-
             this.vet_id = vet_id;
             this.owner_id = owner_id;
             this.pet_id = pet_id;
             this.when = when;
             this.note = note;
-            
+        }
+
+        public Appointment(object selectedItem1, object selectedItem2, object selectedItem3, string text, DateTime when)
+        {
+            this.selectedItem1 = selectedItem1;
+            this.selectedItem2 = selectedItem2;
+            this.selectedItem3 = selectedItem3;
+            this.text = text;
+            this.when = when;
+        }
+
+        public Appointment(object selectedItem1, object selectedItem2, object selectedItem3, object text1, DateTime when)
+        {
+            this.selectedItem1 = selectedItem1;
+            this.selectedItem2 = selectedItem2;
+            this.selectedItem3 = selectedItem3;
+            this.text1 = text1;
+            this.when = when;
         }
     }
 }
--- AppointmentUC.xaml.cs
+++ AppointmentUC.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -23,6 +24,36 @@ namespace VetClinic
         public AppointmentUC()
         {
             InitializeComponent();
+        }
+
+
+
+        private void BtnAppointment_Click(object sender, RoutedEventArgs e)
+        {
+
+            DateTime when = DpAppointment.SelectedDate.Value.Date.Add(TpAppointments.SelectedTime.Value.TimeOfDay);
+
+           
+            Appointment newAppointment = new Appointment(ComboName.SelectedItem, ComboPet.SelectedItem, ComboVet.SelectedItem, TbxNotes.Text, when);
+
+
+            LvAppointment.ItemsSource = Globals.dbContext.Appointments.ToList();
+            Globals.dbContext.Appointments.Add(newAppointment);
+            Globals.dbContext.SaveChanges();
+
+
+        }
+        
+
+        private void UserControl_Loaded(object sender, RoutedEventArgs e)
+        {
+            ComboName.ItemsSource = Globals.dbContext.Owners.ToList();
+            ComboVet.ItemsSource = Globals.dbContext.Users.ToList();
+        }
+
+        private void ComboName_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+                ComboPet.ItemsSource = Globals.dbContext.Pets.ToList();
         }
     }
 }
